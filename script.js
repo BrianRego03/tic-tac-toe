1342a661d595e3ee3e3e8ff62ac547d14b5345de
Brian rego <brian.rego03@gmail.com>
2024-07-23 22:25:13 +0530

    Add functions inside factory to increase or track score and assign signal externally

--- script.js
+++ script.js
@@ -2,10 +2,16 @@ let gameBoard ={
     marks:['a','b','c','d','e','f','g','h','i']
 };
 
-function player(name,signal){
-        return{name,signal,start:0,score:0};
+function player(name){
+    let score=0;
+    const getScore=()=>score;
+    const matchWon=()=>score++;
+
+        return{name,signal:null,start:0,getScore,matchWon};
 }
-
-player1=player();
-player2=player();
-
+playerx=player();
+playery=player();
+playerx.name=prompt("Which player uses X?");
+playerx.signal="X";
+playery.name=prompt("Which player uses O?");
+playery.signal="O";
