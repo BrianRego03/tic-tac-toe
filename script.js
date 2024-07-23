582f2852088fad9ce8f3f529bddbe99657affd86
Brian rego <brian.rego03@gmail.com>
2024-07-24 00:26:37 +0530

    Design function to check inputs and declare winner

--- script.js
+++ script.js
@@ -31,6 +31,7 @@ beginDecider();
 
 function startGame(){
     let count=0;
+    let winner=0;
     while(count<9)
     {
         let xposition;
@@ -41,18 +42,55 @@ function startGame(){
             gameBoard.marks[xposition]=playerx.signal;
             playerx.start--;
             playery.start++;
+            winner=boardChecker();
+            console.log(winner);
+            if(winner=='X')
+                break;
         }
         else{
             yposition=prompt("where should y play?(0-8)");
             gameBoard.marks[yposition]=playery.signal;
             playery.start--;
             playerx.start++;
+            winner=boardChecker();
+            console.log(winner);
+            if(winner=='O')
+                break;
 
         }
         count++;
         
     }
     console.log(gameBoard);
+    if(winner==0)
+        console.log("It's a tie");
+    else if(winner=="X")
+        console.log(`${playerx.name} wins`);
+    else if(winner=='O')
+        console.log(`${playery.name} wins`);
+    else
+        console.log('error');    
 }
 
 startGame();
+
+function boardChecker(){
+    const winnerArray=['012','345','678','036','147','258','048','246'];
+    
+    
+    for(let combo of winnerArray){
+        // console.log(this);
+        let splitArray=combo.split("");
+        console.log(splitArray);
+        if((gameBoard.marks[splitArray[0]]==gameBoard.marks[splitArray[1]])&&
+        (gameBoard.marks[splitArray[1]]==gameBoard.marks[splitArray[2]]))
+        {
+            console.log(`we have a winner ${gameBoard.marks[splitArray[0]]} at
+                 ${splitArray[0]}${splitArray[1]}${splitArray[2]}`);
+            console.log(gameBoard.marks[splitArray[0]]);     
+            return gameBoard.marks[splitArray[0]];      
+        }
+        
+    }
+    return 0;
+}
