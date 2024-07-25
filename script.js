07c674c9a08b584204f5edcc1104cf16df2ee639
Brian rego <brian.rego03@gmail.com>
2024-07-25 12:06:59 +0530

    Move player factory function inside startpageload function

--- script.js
+++ script.js
@@ -2,13 +2,7 @@ let gameBoard ={
     marks:['a','b','c','d','e','f','g','h','i']
 };
 
-function player(name){
-    let score=0;
-    const getScore=()=>score;
-    const matchWon=()=>score++;
 
-        return{name,signal:null,start:0,getScore,matchWon};
-}
 // playerx=player();
 // playery=player();
 
@@ -27,6 +21,15 @@ let count;
 
 
 let startpageLoad=()=>{
+
+    function player(name){
+        let score=0;
+        const getScore=()=>score;
+        const matchWon=()=>score++;
+    
+            return{name,signal:null,start:0,getScore,matchWon};
+    }
+    
     playerx=player();
     playery=player();
     count=0;
